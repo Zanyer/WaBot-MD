6956e630eb319e1d89c4fbfd135b3b315eb260b6
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-01-06 13:42:57 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+pkg update && pkg upgrade
+pkg install git -y
+pkg install nodejs -y
+pkg install ffmpeg -y
+pkg install imagemagick -y
+pkg install yarn
+git clone https://github.com/NeKosmic/WaBot-MD/
+cd WaBot-MD
+yarn
+node main.js
